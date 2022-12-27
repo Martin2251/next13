dcfa1c2c38034f171d9fb06e3d5e697da1c3d604
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-27 17:38:01 +0000

    todo styles

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Todo } from '../../../typings';
 
 type PageProps = {
     params:{
@@ -7,9 +8,34 @@ type PageProps = {
 }
 
 
-const TodoPage = ({params:{todoId}}:PageProps) => {
+const fetchTodo = async (todoId: string) => {
+    //call to 3rd party url 
+
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    );
+
+    const todo = await res.json()
+    return todo;
+
+};
+
+
+const TodoPage = async({params:{todoId}}:PageProps) => {
+
+    const todo:Todo  = await fetchTodo(todoId);
   return (
-    <div>TodoPage : {todoId}</div>
+<div className='p-10 bg-yellow-200 border-2 m-2 shadow-lg'>
+    <p>
+        #{todo.id}: {todo.title}
+    </p>
+    <p> Completed: {todo.completed ? "Yes" : "No"}</p>
+    <p className='border-t border-black mt-5 text-right'>
+        By User: {todo.userId}
+    </p>
+</div>
+
+  
   )
 }
 
